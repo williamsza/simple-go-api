4a5f77a5a921018acc8eccf41b0afee65ab12b2e
williamsza <wyllsza@gmail.com>
2025-08-09 15:03:18 -0300

    Criado metodos createTask, gettAllTasks e deleteTask

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.24.5
 
 require github.com/gin-gonic/gin v1.10.1
 
+require github.com/google/uuid v1.0.0 // indirect
+
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
@@ -21,6 +23,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pborman/uuid v1.2.1
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
--- main.go
+++ main.go
@@ -4,14 +4,81 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	//"github.com/google/uuid"
+	"github.com/pborman/uuid"
+	//routers "simple-go-api/routers/v1"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/v1", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
-		})
-	})
+
+	appRoutes(r)
+
 	r.Run("localhost:3001")
+}
+
+/* ------------------------- */
+
+func appRoutes(router *gin.Engine) *gin.RouterGroup {
+	taskController := newTaskController()
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/tasks", taskController.getAllTasks)
+		v1.POST("/tasks", taskController.createTask)
+		v1.DELETE("/tasks/:id", taskController.deleteTask)
+
+	}
+	return v1
+
+}
+
+/*-----------------------------------------------------------*/
+type taskController struct {
+	tasks []Task
+}
+
+func newTaskController() *taskController {
+	return &taskController{
+		tasks: []Task{},
+	}
+}
+func (tc *taskController) getAllTasks(cxt *gin.Context) {
+	cxt.JSON(http.StatusOK, tc.tasks)
+
+}
+
+func (tc *taskController) createTask(cxt *gin.Context) {
+	task := newTask()
+	if err := cxt.BindJSON(&task); err != nil {
+		return
+	}
+	tc.tasks = append(tc.tasks, *task)
+	cxt.JSON(http.StatusOK, tc.tasks)
+}
+func (tc *taskController) deleteTask(cxt *gin.Context) {
+	id := cxt.Param("id")
+
+	for idx, task := range tc.tasks {
+		if task.ID == id {
+			tc.tasks = append(tc.tasks[0:idx], tc.tasks[idx+1:]...)
+			cxt.JSON(http.StatusOK, tc.tasks)
+			return
+		}
+	}
+	cxt.JSON(http.StatusOK, tc.tasks)
+}
+
+/*-----------------  */
+type Task struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+func newTask() *Task {
+	task := Task{
+		ID: uuid.New(),
+	}
+	return &task
 }
